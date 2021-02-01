7ca3ef06d28ca25ceff3fc8fc0dfb543a1efc2c9
zaxioms <zaxioms@zach-archbox.localdomain>
2021-02-01 09:25:37 -0500

    fix

--- train.py
+++ train.py
@@ -4,14 +4,13 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import transforms
+from torch.autograd import Variable
 from tqdm import tqdm
-import cv2
-from skimage import io
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-
 from models.bbox import bbox
 from utils.dataset import BallDataset
+from PIL import Image
+import matplotlib.pyplot as plt
+from matplotlib import patches
 
 if torch.cuda.is_available():
     device = torch.device("cuda:0")
@@ -21,7 +20,19 @@ else:
     device = torch.device("cpu")
     print("cpu")
 
-EPOCHS = 30
+
+def image_loader(image_name):
+    """load image, returns cuda tensor"""
+    image = Image.open(image_name)
+    loader = transforms.Compose([transforms.Scale(256), transforms.ToTensor()])
+    image = loader(image).float()
+    image = Variable(image, requires_grad=True)
+    image = image.unsqueeze(0)  # this is for VGG, may not be needed for ResNet
+    print("Image tensor shape: ", image.shape)
+    return image.cuda()  # assumes that you're using GPU
+
+
+EPOCHS = 5
 BATCH_SIZE = 32
 loss_arr = []
 bd = BallDataset(
@@ -32,9 +43,7 @@ bd = BallDataset(
 
 train, test = torch.utils.data.random_split(bd, [10000, len(bd) - 10000])
 
-trainset = torch.utils.data.DataLoader(train,
-                                       batch_size=BATCH_SIZE,
-                                       shuffle=True)
+trainset = torch.utils.data.DataLoader(train, batch_size=BATCH_SIZE, shuffle=True)
 testset = torch.utils.data.DataLoader(test, batch_size=BATCH_SIZE, shuffle=True)
 
 net = bbox().to(device)
@@ -59,27 +68,19 @@ y_axis = loss_arr
 fig = px.line(x=x_axis, y=y_axis)
 fig.show()
 
-# test = io.imread("data/validation_images/test1.jpg")
-# resized_image = cv2.resize(test, (256, 256))
-# cv2.imwrite("data/validation_images/test1.jpg", resized_image)
-# x = np.array(cv2.imread("data/validation_images/test1.jpg"))
-
-# x = torch.FloatTensor(x).view(-1, 3, 256, 256)
-# print(x)
-# print(x.shape)
-# net.eval()
-# with torch.no_grad():
-#     out = net(x.to(device)).detach().cpu().numpy()
-# print(out)
-# fig, ax = plt.subplots(1)
-# plt.imshow(test)
-# rect = patches.Rectangle(
-#     (out[0][0], out[0][1]),
-#     out[0][2],
-#     out[0][3],
-#     linewidth=1,
-#     edgecolor="r",
-#     facecolor="none",
-# )
-# ax.add_patch(rect)
-# plt.show()
+net.eval()
+with torch.no_grad():
+    out = net(image_loader("data/resized_images/3375.jpg"))
+print("Test BBox: ", out)
+fig, ax = plt.subplots(1)
+plt.imshow(test)
+rect = patches.Rectangle(
+    (out[0][0], out[0][1]),
+    out[0][2],
+    out[0][3],
+    linewidth=1,
+    edgecolor="r",
+    facecolor="none",
+)
+ax.add_patch(rect)
+plt.show()
